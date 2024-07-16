c08033ba23967e6f4f1c8d3280b0a372c799dd2b
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-07-16 18:47:15 +0300

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Said Tapaev on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
